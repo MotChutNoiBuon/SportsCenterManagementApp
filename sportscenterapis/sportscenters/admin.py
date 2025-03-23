1bcbcd0a580dd026e3a34f1aaa8437cbdb8e83c0
Loc <locn190904@gmail.com>
2025-03-23 20:42:55 +0700

    init admin.site.register(...)

--- sportscenterapis/sportscenters/admin.py
+++ sportscenterapis/sportscenters/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from sportscenters.models import Class, Payment, Enrollment, Progress, Appointment, InternalNews, Notification
 
-# Register your models here.
+admin.site.register(Class)
+admin.site.register(Payment)
+admin.site.register(Enrollment)
+admin.site.register(Progress)
+admin.site.register(Appointment)
+admin.site.register(InternalNews)
+admin.site.register(Notification)
