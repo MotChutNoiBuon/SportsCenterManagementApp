54d92049cf8a233a815f0ba22f742eb050ef3d69
Loc <locn190904@gmail.com>
2025-04-12 16:39:13 +0700

    update frontend

--- SportCenterApp/App.js
+++ SportCenterApp/App.js
@@ -1,20 +1,36 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import LoginScreen from './screens/LoginScreen';
+import HomeScreen from './screens/HomeScreen';
+import ClassDetailScreen from './screens/ClassDetailScreen';
+import { Provider as PaperProvider } from 'react-native-paper';
+
+const Stack = createStackNavigator();
 
 export default function App() {
+  const [accessToken, setAccessToken] = useState(null);
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <PaperProvider>
+      <NavigationContainer>
+        <Stack.Navigator>
+          {!accessToken ? (
+            <Stack.Screen name="Login" options={{ headerShown: false }}>
+              {props => <LoginScreen {...props} setAccessToken={setAccessToken} />}
+            </Stack.Screen>
+          ) : (
+            <>
+              <Stack.Screen name="Home">
+                {props => <HomeScreen {...props} accessToken={accessToken} />}
+              </Stack.Screen>
+              <Stack.Screen name="ClassDetail">
+                {props => <ClassDetailScreen {...props} accessToken={accessToken} />}
+              </Stack.Screen>
+            </>
+          )}
+        </Stack.Navigator>
+      </NavigationContainer>
+    </PaperProvider>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
